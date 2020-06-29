e7208e54d2eb8f0abc61bbfa429d023aaca87cdd
rony646 <rony646@users.noreply.github.com>
2020-06-28 21:23:20 -0300

    new features added

---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import useTimer  from 'react-timer-hook';
+
+
+function Timer() {
+  const time = new Date()
+  time.setSeconds(time.getSeconds() + 1500)
+  let {
+    seconds,
+    minutes,
+    hours,
+    days,
+    startTimer,
+    stopTimer,
+    resume
+  } = useTimer({ autoStart: false, onExpire: () => alert('Timer is over!'), expiryTimestamp: time });
+
+  function parseTime(seconds) {
+    if(seconds <= 9) {
+      return seconds = "0" + seconds;
+    }
+    else {
+      return seconds
+    }
+  }
+  
+  return(
+    <div>
+      <div style={{fontSize: '50px'}}>
+       <span>{parseTime(minutes)}</span>:<span>{parseTime(seconds)}</span>
+      </div>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Pause</button>
+      <button onClick={resume}>Resume</button>
+    </div>
+  )
+}
+
+export default Timer;
