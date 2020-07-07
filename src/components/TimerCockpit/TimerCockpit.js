c97911035330e8a91576ef130f194a023151a7a5
rony646 <rony646@users.noreply.github.com>
2020-07-07 20:16:06 -0300

    fixed play/resume buttons

--- src/components/TimerCockpit/TimerCockpit.js
+++ src/components/TimerCockpit/TimerCockpit.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import parseTime from '../../tools/parseTime'
 
 import classes from './TimerCockpit.css'
+import  icons from '../../../node_modules/font-awesome/css/font-awesome.min.css'
 
 function TimerCockpit(props) {
+    useEffect(() => {
+        console.log(icons['fa-play'])
+    }, [])
     return(
         <div>
             <div className={classes.TimerShow}>
                 <span>{parseTime(props.minutes)}</span>:<span>{parseTime(props.seconds)}</span>
             </div>
-            <button onClick={props.pause} className={[classes.ControlButton].join(' ')} >Pause</button>
-            <button onClick={props.resume} className={classes.ControlButton} >Resume</button>
+            <button 
+                onClick={props.pause} 
+                className={[classes.ControlButton, icons['fa'], icons['fa-stop']].join(' ')}
+            >
+            </button>
+            <button 
+                onClick={props.resume} 
+                className={[classes.ControlButton, icons['fa'], icons['fa-play']].join(' ')}
+            >
+            </button>
         </div>
     )
 }
