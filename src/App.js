095a6b50f6818d7e999ac9883b92432f7b677daa
rony646 <rony646@users.noreply.github.com>
2020-07-12 21:07:51 -0300

    new folder structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import React, { useState } from 'react';
-import Pomodoro from './components/Timers/Pomodoro'
-import ShortBreak from './components/Timers/ShortBreak'
-import LongBreak from './components/Timers/LongBreak'
-import classes from './App.css'
-
-
+import React from 'react';
+import TimerApp from './containers/TimerApp/TimerApp'
 
 function App() {
-  const [timerType, setTimerType] = useState('pomodoro')
-
-  let timer = null
-
-  if(timerType === 'pomodoro') {
-    timer = <Pomodoro />
-  }
-
-  if(timerType === 'shortBreak') {
-    timer = <ShortBreak />
-  }
-
-  if(timerType === 'longBreak') {
-    timer = <LongBreak />
-  }
   
   return (
-  
-    <div className={classes.App}>
-      <h1 className={classes.Title}>Pomodoro Timer</h1>
-      <div>
-        <button className={classes.TimerButton} onClick={() => setTimerType('pomodoro')}>Pomodoro</button>
-        <button className={classes.TimerButton} onClick={() => setTimerType('shortBreak')}>Short Break</button>
-        <button className={classes.TimerButton} onClick={() => setTimerType('longBreak')}>Long Break</button>
-      </div>
-      {timer}
+    <div>
+      <TimerApp />
     </div>
   );
 }
---
+++ src/containers/TimerApp/TimerApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Pomodoro from '../../components/Timers/Pomodoro'
+import ShortBreak from '../../components/Timers/ShortBreak'
+import LongBreak from '../../components/Timers/LongBreak'
+import classes from './TimerApp.css'
+
+
+
+function App() {
+  const [timerType, setTimerType] = useState('pomodoro')
+
+  let timer = null
+
+  if(timerType === 'pomodoro') {
+    timer = <Pomodoro />
+  }
+
+  if(timerType === 'shortBreak') {
+    timer = <ShortBreak />
+  }
+
+  if(timerType === 'longBreak') {
+    timer = <LongBreak />
+  }
+  
+  return (
+  
+    <div className={classes.App}>
+      <h1 className={classes.Title}>Pomodoro Timer</h1>
+      <div>
+        <button className={classes.TimerButton} onClick={() => setTimerType('pomodoro')}>Pomodoro</button>
+        <button className={classes.TimerButton} onClick={() => setTimerType('shortBreak')}>Short Break</button>
+        <button className={classes.TimerButton} onClick={() => setTimerType('longBreak')}>Long Break</button>
+      </div>
+      {timer}
+    </div>
+  );
+}
+
+export default App
