674c75e92c7415a8f2623d073093670addcf97ea
rony646 <rony646@users.noreply.github.com>
2020-07-14 20:57:03 -0300

    more changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import TimerApp from './containers/TimerApp/TimerApp'
 
+
+import icons from '../node_modules/font-awesome/css/font-awesome.min.css'
 import classes from './App.css'
 
 function App() {
   
   return (
-    <div className={classes.Container}>
-      <button>click me</button>
-      <TimerApp />
-    </div>
+    <React.Fragment>
+      <div className={classes.Container}>
+        {/* <button className={classes.InfoButton}>Click me!</button>  To do: make info button and the modal*/} 
+        <TimerApp />
+      </div>
+    </React.Fragment>
   );
 }
 
